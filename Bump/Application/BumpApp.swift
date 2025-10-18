fa58c28727e55a961e63c1f2c7d20db1c0bd4076
Sweety <sweety@MacBook-Pro-290.local>
2025-10-18 12:03:52 -0700

    Initial commit

---
+++ Bump/Application/BumpApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct BumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bump/BumpTests/BumpTests.swift
@@ -0,0 +1,17 @@
+//
+//  BumpTests.swift
+//  BumpTests
+//
+//  Created by Sweety on 10/18/25.
+//
+
+import Testing
+@testable import Bump
+
+struct BumpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
